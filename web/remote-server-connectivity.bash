842468b585b36103754a0ba22ceb454d8962f0fc
anshu aditya <anshuaditya@outlook.com>
2020-09-18 12:22:12 +0530

    new change in remote-server-connectivity script

--- web/remote-server-connectivity.bash
+++ web/remote-server-connectivity.bash
@@ -2,6 +2,17 @@
 # purpose: checks connectivity of a remote host
 # author: anshu aditya
 # date created: 16th Sep 2020
-# last modified: 16th Sep 2020
+# last modified: 18th Sep 2020
 
+echo "Enter the Host ip address:"
+read host_ip
+echo 
+ping -c1 $host_ip > /dev/null
+echo
+	if [ $? -eq 0 ]
+		then
+			echo "Host $host_ip is available"
+		else
+			echo "Host is NOT available"
+	fi
 
